0e8ab434f3b7cbdfdb5bdd09e08848ed2d7d13ff
Caroline Teixeira <carol.barcelos1999@gmail.com>
2026-06-15 23:01:25 -0300

    clean(seeder): remove unused default test user from DatabaseSeeder
    - Removed the boilerplate User creation since the application does not implement or require authentication.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Database\Seeders\ResponsibleSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,16 +10,8 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'), // Ou Hash::make('password')
-        ]);
 
         $this->call(ResponsibleSeeder::class);
     }
